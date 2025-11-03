ca54c0ad61e9931ec76a390472e29b3fdbb8d6f2
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2025-11-03 12:56:05 -0800

    Reformat constraints block  & add docstring

--- src/postgresql/schema/create_ASSOCEVAMPSET.sql
+++ src/postgresql/schema/create_ASSOCEVAMPSET.sql
@@ -1,5 +1,6 @@
 -- ASSOCEVAMPSET TABLE PARAMETRIC SCHEMA
 -- Association of events and amplitude sets in the ANSS/CISN database parametric schema
+-- edited to conform with the structuring of the majority of table defitions
 -- auth: Renate Hartog
 -- editor: Nathan T. Stevens
 -- orgs: PNSN / AQMS-SWG
@@ -11,28 +12,31 @@ AMPSETTYPE  VARCHAR(20) not null,
 EVID BIGINT not null,
 SUBSOURCE VARCHAR(8) not null,
 ISVALID SMALLINT not null,
-LDDATE TIMESTAMP default (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
+LDDATE TIMESTAMP default (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+CONSTRAINT ASSOCEVAMPSETKEY01 PRIMARY KEY (ampsetid, ampsettype),
+CONSTRAINT ASSOCEVAMPSETKEY02 FOREIGN KEY (ampsettype) REFERENCES ampsettypes(ampsettypes),
+CONSTRAINT ASSOCEVAMPSETKEY03 FOREIGN KEY (evid) REFERENCES event(evid)
 );
 
-alter table ASSOCEVAMPSET add constraint assocevampsetkey01 primary key
-(ampsetid, ampsettype);
+-- alter table ASSOCEVAMPSET add constraint assocevampsetkey01 primary key
+-- (ampsetid, ampsettype);
 
-alter table ASSOCEVAMPSET add constraint assocevampsetkey02 foreign key 
-(ampsettype)
-references ampsettypes(ampsettype);
+-- alter table ASSOCEVAMPSET add constraint assocevampsetkey02 foreign key 
+-- (ampsettype)
+-- references ampsettypes(ampsettype);
 
-alter table ASSOCEVAMPSET add constraint assocevampsetkey03 foreign key
-(evid)
-references event(evid);
+-- alter table ASSOCEVAMPSET add constraint assocevampsetkey03 foreign key
+-- (evid)
+-- references event(evid);
 
 
-comment on table ASSOCEVAMPSET is 'This table associates amplitude sets with an event';
-comment on column ASSOCEVAMPSET.AMPSETID is 'References ampset.ampsetid.';
-comment on column ASSOCEVAMPSET.AMPSETTYPE is 'References ampset.ampsettype.';
-comment on column ASSOCEVAMPSET.EVID is 'References event.evid.';
-comment on column ASSOCEVAMPSET.ISVALID is 'Has value 1 if preferred set, 0 otherwise ';
-comment on column ASSOCEVAMPSET.SUBSOURCE is 'Name tag of the application creating the set (e.g. AmpGen AmpGenPp Gmp2Db)';
-comment on column ASSOCEVAMPSET.LDDATE is 'Date record created in UTC';
+-- comment on table ASSOCEVAMPSET is 'This table associates amplitude sets with an event';
+-- comment on column ASSOCEVAMPSET.AMPSETID is 'References ampset.ampsetid.';
+-- comment on column ASSOCEVAMPSET.AMPSETTYPE is 'References ampset.ampsettype.';
+-- comment on column ASSOCEVAMPSET.EVID is 'References event.evid.';
+-- comment on column ASSOCEVAMPSET.ISVALID is 'Has value 1 if preferred set, 0 otherwise ';
+-- comment on column ASSOCEVAMPSET.SUBSOURCE is 'Name tag of the application creating the set (e.g. AmpGen AmpGenPp Gmp2Db)';
+-- comment on column ASSOCEVAMPSET.LDDATE is 'Date record created in UTC';
 
 
 -- create or replace public synonym ASSOCEVAMPSET for ASSOCEVAMPSET;
