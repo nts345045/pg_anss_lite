dbf220a24a95b97fca806f318cbd0c4364190506
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2025-11-04 03:12:49 -0800

    Simplify to single entry

--- postgresql/extended_schema/xcorr/create_XCORR.sql
+++ postgresql/extended_schema/xcorr/create_XCORR.sql
@@ -10,11 +10,9 @@
 -- JEVID: Test event ID (j^th)
 -- JARID: Test arrival ID
 -- SAMP_RATE: sampling rate of vectors cross correlated
--- CCP: Maximum positive cross-correlation value
--- CCN: Maximum negative cross-correlation value
--- SHP: Shift in samples corresponding to CCP (signed)
--- SHN: Shift in samples corresponding to CCN (signed)
--- SHMAX: Maximum shift tested in samples (unsigned)
+-- CC_VAL: Cross correlation coefficient value
+-- CC_SHIFT: Cross correlation shift of test data corresponding to CC_VAL in samples
+-- SHMAX: Unsigned maximum shift of test data assessed in samples 
 -- LDDATE: Date loaded into database
 
 CREATE TABLE XCORR (
@@ -23,19 +21,32 @@ CREATE TABLE XCORR (
     JEVID BIGINT,
     JARID BIGINT,
     SAMP_RATE REAL NOT NULL,
-    CCP REAL,
-    CCN REAL,
-    SHP INTEGER,
-    SHN INTEGER,
+    CC_VAL REAL,
+    CC_SHIFT INTEGER,
     SHMAX INTEGER NOT NULL,
     LDDATE TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
-    CONSTRAINT XCORR01 CHECK (ccp >= 0 AND ccp <= 1),
-    CONSTRAINT XCORR02 CHECK (ccn <= 0 AND ccn >= -1),
-    CONSTRAINT XCORR03 CHECK (ABS(shp) <= shmax),
-    CONSTRAINT XCORR04 CHECK (ABS(shn) <= shmax),
-    CONSTRAINT XCORRKEY01 PRIMARY KEY (ievid, iarid, jevid, jarid),
+    CONSTRAINT XCORR01 CHECK (cc_val >= -1 AND cc_val <= 1),
+    CONSTRAINT XCORR02 CHECK (ABS(cc_shift) <= shmax),
+    CONSTRAINT XCORR03 CHECK (samp_rate > 0),
     CONSTRAINT XCORRKEY02 FOREIGN KEY (ievid) REFERENCES event(evid),
     CONSTRAINT XCORRKEY03 FOREIGN KEY (iarid) REFERENCES arrival(arid),
     CONSTRAINT XCORRKEY04 FOREIGN KEY (jevid) REFERENCES event(evid),
     CONSTRAINT XCORRKEY05 FOREIGN KEY (jarid) REFERENCES arrival(arid)
 );
+
+
+
+
+    -- EXPERIMENTAL CONSTRAINTS
+
+-- CCP: Maximum positive cross-correlation value
+-- CCN: Maximum negative cross-correlation value
+-- SHP: Shift in samples corresponding to CCP (signed)
+-- SHN: Shift in samples corresponding to CCN (signed)
+-- SHMAX: Maximum shift tested in samples (unsigned)
+-- TODO
+
+    -- CONSTRAINT XCORR01 CHECK (array_ndims(cc_vals) = 1),
+    -- CONSTRAINT XCORR02 CHECK (array_ndims(cc_shifts) = 1),
+    -- CONSTRAINT XCORR03 CHECK (array_length(cc_vals, 1) = array_length(cc_shifts,1)),
+    -- CONSTRAINT XCORRKEY01 PRIMARY KEY (ievid, iarid, jevid, jarid),
